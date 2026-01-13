df90ec387424b98b4b2f9097a081dd55fd3cd414
Kushagra Varshney <devkussu43@gmail.com>
2026-01-13 10:04:28 +0530

    Day11 work

---
+++ javaTraining/Day11/Practice/Main1.java
@@ -0,0 +1,41 @@
+package Day11.Practice;
+
+
+interface InterfaceName {
+    void mth1();
+    void mth2();
+
+    default void mth3() {
+        System.out.println("Default mth inside interface");
+    }
+    int a = 20;
+
+    static void mth4() {
+
+    }
+}
+
+class Parent implements InterfaceName{
+    
+    public void mth1() {
+        System.out.println("mth1");
+    }
+
+    public void mth2() {
+        System.out.println("mth2");
+    }
+
+    void setA(int a) {
+        super.a = a;
+    }
+}
+
+public class Main1 {
+    public static void main(String[] args) {
+        Parent p = new Parent();
+        p.mth1();
+        p.mth2();
+        p.mth3();
+        System.out.println(InterfaceName.a);
+    }
+}
