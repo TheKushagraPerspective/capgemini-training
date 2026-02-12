3a76f21b6e49e83084360771eed4efdc7b5f2652
Kushagra Varshney <devkussu43@gmail.com>
2026-02-12 11:34:05 +0530

    Hibernate Begins

--- All-Capg-Workspaces/Capg-Adv-Java-Workspaces/Hibernate01_Student/src/com/capg/hibernate/Student.java
+++ All-Capg-Workspaces/Capg-Adv-Java-Workspaces/Hibernate01_Student/src/com/capg/hibernate/Student.java
@@ -1,5 +1,38 @@
 package com.capg.hibernate;
 
-public class Student {
+import java.io.Serializable;
 
+public class Student implements Serializable {
+	private int id;
+	private String name;
+	private double marks;
+	
+//	constructor
+	public Student() {
+		
+	}
+	
+//	setter / getter methods
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double getMarks() {
+		return marks;
+	}
+	public void setMarks(double marks) {
+		this.marks = marks;
+	}
+	
+	
 }
