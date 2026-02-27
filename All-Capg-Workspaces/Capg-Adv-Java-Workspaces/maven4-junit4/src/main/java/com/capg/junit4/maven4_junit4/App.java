6895643f8e0e04bf13f82785f760abe754a6d17b
Kushagra Varshney <devkussu43@gmail.com>
2026-02-27 10:09:28 +0530

    spring mvc project

---
+++ All-Capg-Workspaces/Capg-Adv-Java-Workspaces/maven4-junit4/src/main/java/com/capg/junit4/maven4_junit4/App.java
@@ -0,0 +1,13 @@
+package com.capg.junit4.maven4_junit4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
